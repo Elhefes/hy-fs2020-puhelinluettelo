5aeffbb145d0a9f6d76dc4842c612d46f8c163bd
henripalin <henri.j.palin@gmail.com>
2020-07-21 23:14:09 +0300

    3.4 done

--- index.js
+++ index.js
@@ -40,6 +40,13 @@ let persons = [
       }
   })
 
+  app.delete('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    persons = persons.filter(person => person.id !== id)
+  
+    response.status(204).end()
+  })
+
   app.get('/api/persons', (req, res) => {
     res.json(persons)
   })
