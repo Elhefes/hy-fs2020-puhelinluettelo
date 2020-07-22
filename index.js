70847dbf1d09c1962a8a09efaf402f17ccdc3242
henripalin <henri.j.palin@gmail.com>
2020-07-22 17:17:03 +0300

    3.6 done

--- index.js
+++ index.js
@@ -49,10 +49,16 @@ let persons = [
   app.post('/api/persons', (request, response) => {
     const body = request.body
   
-    if (!body.name) {
+    if (!body.name || !body.number) {
       return response.status(400).json({ 
         error: 'content missing' 
       })
+    }
+
+    if (persons.map(p => p.name).includes(body.name)) {
+      return response.status(400).json({
+        error: 'name must be unique' 
+      });
     }
   
     const person = {
