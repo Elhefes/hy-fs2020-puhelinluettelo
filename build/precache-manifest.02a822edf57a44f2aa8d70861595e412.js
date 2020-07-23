71d3c385b9cac99cc7ece45ba04a45faa4f197b2
henripalin <henri.j.palin@gmail.com>
2020-07-23 21:51:03 +0300

    3.11 done

---
+++ build/precache-manifest.02a822edf57a44f2aa8d70861595e412.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e9b4a2c259b556420ce70ab5e7c68e7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1dc2690ece5e315bacde",
+    "url": "/static/css/main.6c2a2d5e.chunk.css"
+  },
+  {
+    "revision": "7db88a272722b7822afa",
+    "url": "/static/js/2.592545cb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.592545cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1dc2690ece5e315bacde",
+    "url": "/static/js/main.ed0d052c.chunk.js"
+  },
+  {
+    "revision": "f0be2d6b237b3fb64219",
+    "url": "/static/js/runtime-main.24b8e7b7.js"
+  }
+]);
